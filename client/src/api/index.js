1da14a5cfc4873b3c99014e421bb653f5fa17587
Shikhar-Rastogi <shikharrastogi021@gmail.com>
2026-01-07 21:10:18 +0530

    Deployment Changes

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true, // only if using cookies
 });
+
+export default API;
 
 // ---------- Auth ----------
 export const UserSignUp = (data) =>
