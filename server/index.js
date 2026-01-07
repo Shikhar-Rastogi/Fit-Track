ae4f547cee677b74ce759b346afad35ae0cfa735
Shikhar-Rastogi <shikharrastogi021@gmail.com>
2026-01-07 21:44:39 +0530

    Deployment Changes

--- server/index.js
+++ server/index.js
@@ -16,12 +16,13 @@ const app = express();
 app.use(
   cors({
     origin: [
-      "http://localhost:5173", 
-      "https://fit-track-hqya-6fw64fd6o-shikhar-rastogis-projects-694dda53.vercel.app", 
+      "http://localhost:5173",
+      /\.vercel\.app$/,  
     ],
     credentials: true,
   })
 );
+
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
